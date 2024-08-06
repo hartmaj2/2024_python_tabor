95ec2849750c8e7e04a4ceeed1d6f4c939c6b3e9
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-06 21:19:27 +0200

    Add riddle on loops

---
+++ Tabor_Code/W1D3/0Hadanky/rozdil_cykly.py
@@ -0,0 +1,22 @@
+# Jaky je rozdil mezi tremi nasledujicimi cykly?
+
+seznam = ["kocka","vlocka","cocka","ocka"]
+
+print("1.")
+i = 0
+while i < 4:
+    print(seznam[i])
+    i = i + 1
+
+
+print("2.")
+# 2.
+for i in range(4):
+    print(seznam[i])
+
+
+print("3.")
+# 3.
+for prvek in seznam:
+    print(prvek)
+
