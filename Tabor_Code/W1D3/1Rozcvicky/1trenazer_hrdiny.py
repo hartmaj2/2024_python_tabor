3571482c0535a541809b87b27ee31910ff81cfd9
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-06 21:20:17 +0200

    Rename trenazer hrdiny

--- Tabor_Code/W1D3/1Rozcvicky/1trenazer_hrdiny.py
+++ Tabor_Code/W1D3/1Rozcvicky/1trenazer_hrdiny.py
@@ -3,15 +3,25 @@
 # Pote dostanes moznost, delat nejake aktivity (ktere ti budou upravovat tve hodnoty hrdiny)
 # Nasledujici body muzes ignorovat ale mohou ti pomoct
 
-# TODO: Nacti jmeno hrdiny od uzivagele
+# TODO: Nacti jmeno hrdiny od uzivatele
 
-# TODO: Vytvor promenne, ktere budou uchovavat hrdinovy hodnoty (zdravi, sila, penize)
+# TODO: Vytvor promenne, ktere budou uchovavat hrdinovy hodnoty (zdravi, sila, kolik ma penez)
 
 # TODO: Vytiskni uzivately moznosti toho, co muze delat
+# napriklad:
+# 1. Jit do posilovny
+# 2. Mlatit skrety
+# 3. Pracovat
+# 4. Do hospody hrat kostky 
 
 # TODO: Ziskej od uzivatele jeho volbu a podle toho uprav jeho hodnoty (uber zdravi, pridej penize atd)
+# napriklad:
+# 
 
 # TODO: Udelej, abys mohl toto opakovat porad dokola, dokud uzivatel nezada volbu konec
 
 
 # BONUS TODO: Pridej prvky nahody (nahodny pocet penez atd.)
+
+# BONUS TODO: Pridej inventar z cviceni s obchodem do teho hry (nejlepe udelej funkci obchod, ktera zabali tvuj program s obchodem do sve definice)
+# Pribyde tedy moznost 5. Jit nakupovat predmetym
