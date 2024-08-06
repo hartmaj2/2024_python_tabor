ad3feafd74b5101181dcf50b61646f3f32352cd3
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-06 21:41:23 +0200

    Add our ciphers and make other minor changes

--- Tabor_Code/W1D2/2A_Seznamy/1cviceni_seznamy.py
+++ Tabor_Code/W1D2/2A_Seznamy/1cviceni_seznamy.py
@@ -16,11 +16,7 @@ inventory = []
 
 # TODO: nech uzivatele zvolit poradi predmetu, ktery chce koupit a pridej mu ten predmet do inventare
 
-# BONUS TODO: pridej i moznost buy a sell
-# 1. zeptej se uzivatele, zda chce kupovat nebo prodavat
-# 2. podle jeho volby:
-    # pokud si zvolil nakup -> zobraz seznam predmetu v obchode
-    #   ...          prodej -> zobraz seznam predmetu z inventare
+
 
 
 
